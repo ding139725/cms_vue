5ec34e62bd0a0db000db72d78347aeb088966922
@alex159 <1397251417@qq.com>
2020-11-17 19:10:09 +0800

    初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'/',
+    redirect:'/login'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+router.beforeEach((to,from,next)=>{
+  if(to.path=='/login'){
+    next();
+  }
+  const token = localStorage.getItem('token');
+  if(token){
+    next()
+  }else{
+    next('/login')
+  }
+})
+
+export default router
